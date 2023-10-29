38d7d747eeb9805fe384491ed89b04c69dfb04e1
Tatsukawa <taihei_tatsukawa@commude.co.jp>
2023-10-29 13:29:18 +0900

    WIP 映画用カード

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -10,17 +10,54 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
       <img
         className="
             duration
-            h-full
+            h-[12vw]
             w-full
             cursor-pointer
             rounded-md
             object-cover
             shadow-xl
             transition
+            delay-300
+            group-hover:opacity-90
+            sm:group-hover:opacity-0
         "
         src={data.thumbnailUrl}
         alt="Thumbnail"
       />
+      <div
+        className="
+          invisible
+          absolute
+          top-0
+          z-10
+          w-full
+          scale-0
+          opacity-0
+          transition
+          delay-300
+          duration-200
+          group-hover:-translate-y-[6vw]
+          group-hover:translate-x-[2vw]
+          group-hover:scale-110
+          group-hover:opacity-100
+          sm:invisible
+        "
+      >
+        <img
+          className="
+              duration
+              h-[12vw]
+              w-full
+              cursor-pointer
+              rounded-t-md
+              object-cover
+              shadow-xl
+              transition
+            "
+          src={data.thumbnailUrl}
+          alt="Thumbnail"
+        />
+      </div>
     </div>
   );
 };
